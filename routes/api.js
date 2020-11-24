bc148c79bd07d6519d4953eda18c9f1861fc5917
fredlc <fredlefevre@outlook.com>
2020-11-24 15:29:05 -0500

    Connected to mongodb and created issue schema

--- routes/api.js
+++ routes/api.js
@@ -1,35 +1,50 @@
 /*
-*
-*
-*       Complete the API routing below
-*
-*
-*/
+ *
+ *
+ *       Complete the API routing below
+ *
+ *
+ */
 
-'use strict';
+"use strict";
+
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+mongoose.connect(process.env.DB_URI || "mongodb://localhost/issue-tracker", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const issueSchema = new Schema({
+  issueTitle: { type: String, required: true },
+  issueText: { type: String, required: true },
+  createdOn: { type: Date, default: Date.now },
+  updatedOn: { type: Date, default: Date.now },
+  createdBy: { type: String, required: true },
+  assignedTo: String,
+  open: { type: Boolean, default: true },
+  statusText: String,
+});
+
+const Issue = mongoose.model("Issue", issueSchema);
 
 module.exports = function (app) {
+  app
+    .route("/api/issues/:project")
 
-  app.route('/api/issues/:project')
-  
-    .get(function (req, res){
+    .get(function (req, res) {
       let project = req.params.project;
-      
     })
-    
-    .post(function (req, res){
+
+    .post(function (req, res) {
       let project = req.params.project;
-      
     })
-    
-    .put(function (req, res){
+
+    .put(function (req, res) {
       let project = req.params.project;
-      
     })
-    
-    .delete(function (req, res){
+
+    .delete(function (req, res) {
       let project = req.params.project;
-      
     });
-    
 };
